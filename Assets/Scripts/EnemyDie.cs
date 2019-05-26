e3737af4138575697a71633a2c96c07ee5b3e7f2
Ali Zaib <alizaib1217@gmail.com>
2019-05-27 01:20:52 +0500

    first game

---
+++ Assets/Scripts/EnemyDie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDie : MonoBehaviour {
+
+	// Use this for initialization
+	private EnemyMovement EnemyObject;
+	
+	void Start ()
+	{
+
+		EnemyObject = gameObject.transform.GetComponentInParent<EnemyMovement>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{//Debug.Log(("Kill Enemy"));
+		//if (other.IsTouchingLayers(LayerMask.GetMask("Player")))
+		if(other.gameObject.name=="Player")
+		{
+            			
+			EnemyObject.Die();
+			
+			Destroy((this.gameObject));
+			Debug.Log(("Kill Enemy"));
+			
+		}
+
+
+	}
+}
